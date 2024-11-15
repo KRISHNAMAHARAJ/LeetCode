9feb036bb976e765183e738c434c93333969b915
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-11-15 05:43:56 +0530

    Time: 8 ms (13.66%), Space: 11.8 MB (10.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // bool isPalindrome(int x) {
+    //      if (x < 0) {
+    //         return false;
+    //     }
+
+    //     long long reversed = 0;
+    //     long long temp = x;
+
+    //     while (temp != 0) {
+    //         int digit = temp % 10;
+    //         reversed = reversed * 10 + digit;
+    //         temp /= 10;
+    //     }
+
+    //     return (reversed == x);
+    // }
+
+
+    bool isPalindrome(int x) {
+
+        string str = to_string(x);
+
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+
+        return (str == rev);
+    }
+};
