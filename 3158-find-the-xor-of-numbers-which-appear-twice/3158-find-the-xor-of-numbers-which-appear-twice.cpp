8b5d39ddd96aadd4bf7dfaf8a4f0ba14e7434ca0
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-11-25 14:30:36 +0530

    Time: 3 ms (43.07%), Space: 28.2 MB (53.97%) - LeetHub

--- 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.cpp
+++ 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.cpp
@@ -5,12 +5,12 @@ public:
     for(auto it:nums){
         mp[it]++;
     }
-    int h=0;    
+    int result=0;    
     for(auto it:mp){
         if(it.second==2){ 
-                h^=it.first;
+                result^=it.first;
         }
     }
-    return h;
+    return result;
     }
 };
