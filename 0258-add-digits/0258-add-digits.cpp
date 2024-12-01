3dd5151494323dc80e0a0c42416093c4c673ab7b
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-01 19:41:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (6.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    /*int addDigits(int num) {
-        if(num ==0)
+    int addDigits(int num) {
+        if(num ==0)               /////DIGITAL ROOT concept///
             return 0;
         else if(num % 9 == 0)
             return 9;
         else 
             return num %9;        
     }
-    */
-    int addDigits(int num) {
-    int sum=0;
-    while(num>9)
-    {
-        while(num)
-        {
-            sum+=(num%10);
-            num/=10;
-        }
-        num=sum;
-        sum=0;
-    }
-    return num;
-}
+
 };
 
 
