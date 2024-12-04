ee8bc8d319066288e8be3d9d6a09cefc6be2237b
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-04 08:42:14 +0530

    Time: 0 ms (100%), Space: 12.2 MB (50.29%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,13 +2,35 @@ class Solution {
 public:
     string reverseWords(string s) {
     int start = 0, n = s.size();
-    for (int end = 0; end <= n; ++end) {
-        // When we hit a space or reach the end of the string
+    for (int end = 0; end <= n; end++) {
         if (end == n || s[end] == ' ') {
-            reverse(s.begin() + start, s.begin() + end); // Reverse the word
-            start = end + 1; // Move to the next word
+            reverse(s.begin() + start, s.begin() + end); 
+            start = end + 1; 
         }
     }
     return s;
 }
 };
+
+
+
+
+////// 1st try - fail ///////
+
+/*
+int n = s.length();
+        string result;
+        vector<int> vec;
+        for(int i=0; i<n; i++){
+            while(s[i] != ' '){
+                vec.push_back(s[i]);
+            }
+            reverse(vec.begin(), vec.end());
+            for(int j=0; j<vec.size(); j++){
+                result += vec[j];
+            }
+            vec.clear();
+        }
+        return result;
+*/
+
