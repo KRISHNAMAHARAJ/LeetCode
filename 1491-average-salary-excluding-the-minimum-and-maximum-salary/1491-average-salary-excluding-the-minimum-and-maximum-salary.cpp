883a3f977a9860dc56f1ed7d6fb5c2785fe2ba2d
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-07 13:01:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (11.15%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -3,6 +3,26 @@ public:
     double average(vector<int>& salary) {
         
         int n=salary.size();
+        double sum=0;
+        int minm = INT_MAX, maxm = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            minm = min(minm, salary[i]);
+            maxm = max(maxm, salary[i]);
+            sum += salary[i];
+        }
+        sum = sum - minm - maxm;
+        return sum/(n-2);
+    }
+};
+
+
+//////////// NOW, in O(nlogn) Time Complexity //////////
+////This is basic approach.
+/*
+double average(vector<int>& salary) {
+        
+        int n=salary.size();
         sort(salary.begin(), salary.end());
 
         double sum=0;
@@ -11,4 +31,4 @@ public:
         }
         return sum/(n-2);
     }
-};
+*/
