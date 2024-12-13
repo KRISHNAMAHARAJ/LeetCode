11adae97d4a59d899af7573fe1aa912bce7ad34b
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-13 11:21:00 +0530

    Time: 15 ms (12.05%), Space: 24.6 MB (5.48%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,10 +2,28 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
+        map<int, int> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+
+        for(auto it : mpp){
+            if(it.second == 1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+    int singleNumber(vector<int>& nums) {
+        
         int result=0;
         for(int i=0; i<nums.size(); i++){
             result ^= nums[i];
         }
         return result;
     }
-};
+*/
+    
