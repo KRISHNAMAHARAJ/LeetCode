41ce60d09b51bd1cde2daebf232b8b01bbbeb555
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-14 14:48:28 +0530

    Time: 0 ms (100%), Space: 26.3 MB (11.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        
+        int left=0, right = nums.size() -1;
+
+        while(left < right){
+            int mid = (left + right)/2;
+            
+            // Ensure mid is even to compare pairs
+            if (mid % 2 == 1) mid--;  // Make sure mid is even
+
+            // If the pair is correct, move to the right half
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+            // Otherwise, move to the left half
+                right = mid;
+            }
+        }
+        return nums[left];  // The single element
+    }
+};
+
+////// Above method BINARY SEARCH TC:- O(logn) and SC:- O(1)
+
+
+
+//////// USING XOR METHOD TC:- O(n) and SC:- O(1)
+/*
+    int singleNonDuplicate(vector<int>& nums) {
         int result=0;
         for(int i=0;i<nums.size(); i++){
             result ^= nums[i];
         }
         return result;
     }
-};
+*/
 
 
 
