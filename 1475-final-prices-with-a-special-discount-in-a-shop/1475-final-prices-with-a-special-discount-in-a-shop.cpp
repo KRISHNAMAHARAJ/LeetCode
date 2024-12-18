564eab3fbe8f5631e28f1f840fe78e8549ad703f
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-18 19:03:59 +0530

    Time: 0 ms (100%), Space: 14.1 MB (17.75%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,7 +4,7 @@ public:
         
         int n= prices.size();
         vector<int> result;
-
+                                    //////// THis method has TC:- O(n^2) and SC:- O(n)
         for(int i=0; i<n; i++){
             int val = prices[i];
             bool discountApplied = false;
@@ -23,3 +23,28 @@ public:
         return result;
     }
 };
+
+
+
+///////////// By Using Stack we can do in optimized way TC:- O(n) and SC:- O(1)
+/*
+
+    vector<int> finalPrices(vector<int>& prices) {
+    int n = prices.size();
+    stack<int> st; // Stack to store indices
+
+    for (int i = 0; i < n; i++) {
+        // While stack is not empty and current price <= price at the top of the stack
+        while (!st.empty() && prices[i] <= prices[st.top()]) {
+            prices[st.top()] -= prices[i]; // Apply the discount
+            st.pop(); // Remove the index since it’s processed
+        }
+        st.push(i); // Push the current index onto the stack
+    }
+
+    // Return the modified prices array
+    return prices;
+}
+
+
+*/
