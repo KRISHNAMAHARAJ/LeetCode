f19c5982ca95c919afd2e2837afe165eb6d0c9f0
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-19 23:16:24 +0530

    Time: 31 ms (47.11%), Space: 42.3 MB (45.57%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -2,55 +2,6 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
         
-        const int MAX_VALUE = 100001;
-        vector<int> hash(MAX_VALUE, 0);
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] %2 == 0){
-                hash[nums[i]]++;
-            }
-        }
-        int maxFreq =0;
-        int result=-1;
-
-        for (int i=0; i<MAX_VALUE; i += 2) {
-
-            if (hash[i] > maxFreq) {
-                maxFreq = hash[i];
-                result = i;
-            }
-        }
-        return result;
-    }
-};
-
-    int mostFrequentEven(vector<int>& nums) {
-        
-        const int MAX_VALUE = 100001;
-        vector<int> hash(MAX_VALUE, 0);
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] %2 == 0){
-                hash[nums[i]]++;
-            }
-        }
-        int maxFreq =0;
-        int result=-1;
-
-        for (int i=0; i<MAX_VALUE; i += 2) {
-
-            if (hash[i] > maxFreq) {
-                maxFreq = hash[i];
-                result = i;
-            }
-        }
-        return result;
-    }
-
-
-/*
-    int mostFrequentEven(vector<int>& nums) {
-        
         map<int, int> mp;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] %2 == 0){
@@ -67,6 +18,32 @@ public:
             if (freq > maxFreq || (freq == maxFreq && num < result)) {
                 maxFreq = freq;
                 result = num;
+            }
+        }
+        return result;
+    }
+};
+
+////// USING HASH TABLE 
+/*
+    int mostFrequentEven(vector<int>& nums) {
+        
+        const int MAX_VALUE = 100001;
+        vector<int> hash(MAX_VALUE, 0);
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] %2 == 0){
+                hash[nums[i]]++;
+            }
+        }
+        int maxFreq =0;
+        int result=-1;
+
+        for (int i=0; i<MAX_VALUE; i += 2) {
+
+            if (hash[i] > maxFreq) {
+                maxFreq = hash[i];
+                result = i;
             }
         }
         return result;
