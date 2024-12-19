fc0e82de7a86aae653c24a13d991182e62e97fef
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-19 23:14:04 +0530

    Time: 42 ms (24.95%), Space: 133.3 MB (6.69%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -2,6 +2,55 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
         
+        const int MAX_VALUE = 100001;
+        vector<int> hash(MAX_VALUE, 0);
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] %2 == 0){
+                hash[nums[i]]++;
+            }
+        }
+        int maxFreq =0;
+        int result=-1;
+
+        for (int i=0; i<MAX_VALUE; i += 2) {
+
+            if (hash[i] > maxFreq) {
+                maxFreq = hash[i];
+                result = i;
+            }
+        }
+        return result;
+    }
+};
+
+    int mostFrequentEven(vector<int>& nums) {
+        
+        const int MAX_VALUE = 100001;
+        vector<int> hash(MAX_VALUE, 0);
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] %2 == 0){
+                hash[nums[i]]++;
+            }
+        }
+        int maxFreq =0;
+        int result=-1;
+
+        for (int i=0; i<MAX_VALUE; i += 2) {
+
+            if (hash[i] > maxFreq) {
+                maxFreq = hash[i];
+                result = i;
+            }
+        }
+        return result;
+    }
+
+
+/*
+    int mostFrequentEven(vector<int>& nums) {
+        
         map<int, int> mp;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] %2 == 0){
@@ -22,4 +71,4 @@ public:
         }
         return result;
     }
-};
+*/
