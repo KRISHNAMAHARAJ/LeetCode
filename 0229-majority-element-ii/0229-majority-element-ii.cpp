06a21377b27f234cb5b6dbecb3378150b127585c
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-21 22:37:15 +0530

    Time: 0 ms (100%), Space: 19.9 MB (37.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -18,3 +18,27 @@ public:
         return result;
     }
 };
+
+
+////////// By this method Using Moore Voting algorithm TC:- O(n) and SC:- O(1)
+/*
+
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+*/
