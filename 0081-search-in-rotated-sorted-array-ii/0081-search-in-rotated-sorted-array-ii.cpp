38c35a5563136ae30192155411e6b09094f52490
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-23 03:26:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (39.71%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,9 +2,36 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size(); i++){          ///// NAIVE APPROACH TC:- O(n)
             if(nums[i] == target) return true;
         }
         return false;
     }
 };
+
+
+//////// USING BINARY SEARCH TC:- O(logn)
+/*
+    bool search(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[low] == nums[mid]) {
+                low++;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+*/
