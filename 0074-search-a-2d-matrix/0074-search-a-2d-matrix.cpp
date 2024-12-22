51a71de1e0241b25188cfc131a8c4c95b147ea64
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-23 03:40:07 +0530

    Time: 0 ms (100%), Space: 13.2 MB (32.02%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();         
+                                            //// TC:- O(n*m) 
+        for(int i =0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+/*
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int i =0, j=m-1;
+
+        while(i<=j){
+                        /// Starting from the top right Corner
+            if(matrix[i][j] == target) return true;
+
+            else if(matrix[i][j] > target){
+                j--;     /// Move Left
+            }
+            else {
+                i++;    /// Move Down
+            }        
+        }
+        return false;
+    }
+*/    
