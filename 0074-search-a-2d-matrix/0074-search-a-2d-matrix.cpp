3c8cabe2cd33a646c064fc7bacd5c709ccca95a8
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-23 03:46:18 +0530

    Time: 0 ms (100%), Space: 13.3 MB (32.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,29 +3,11 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         int m = matrix.size();
-        int n = matrix[0].size();         
-                                            //// TC:- O(n*m) 
-        for(int i =0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-};
-
-
-/*
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        int m = matrix.size();
         int n = matrix[0].size();
 
-        int i =0, j=m-1;
+        int i =0, j=n-1;
 
-        while(i<=j){
+        while(i<m && j>=0){
                         /// Starting from the top right Corner
             if(matrix[i][j] == target) return true;
 
@@ -38,4 +20,23 @@ public:
         }
         return false;
     }
-*/    
+};
+  
+
+/////////// USING NAIVE APPROACH TC:- O(n*m) 
+/*
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();         
+                                            //// TC:- O(n*m) 
+        for(int i =0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+*/
