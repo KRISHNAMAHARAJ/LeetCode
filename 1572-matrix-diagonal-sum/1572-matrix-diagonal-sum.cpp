cdb430240d10d6578b48f65d80caa50daa584c71
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2024-12-24 22:42:33 +0530

    Time: 0 ms (100%), Space: 14.9 MB (23.38%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int sum=0;
+        for(int i=0, j=0; i<n, j<m; i++, j++){
+
+            if(i == j){
+                sum += mat[i][j];
+            }
+        }
+        for(int i=0, j=n-1; i<m, j>=0; i++, j--){
+            sum += mat[i][j];
+        }
+
+        if(m % 2 != 0){
+            sum -= mat[m/2][n/2];
+        }
+        return sum;
+    }
+};
