fc3edc44994a44a8520b95ec40669a863a4b1aa5
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-17 20:08:28 +0530

    Time: 4 ms (23.27%), Space: 40.1 MB (86.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
+        int countZero = 0;          ///// Using Brute Force but more efficient
+        int product = 1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                countZero++;
+            }else{
+                product *= nums[i];
+            }
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(countZero>1){
+                nums[i]=0;
+            }else if((countZero==1) && nums[i]!=0){
+                nums[i]=0;
+            }else if ((countZero==1) && nums[i]==0){
+                
+                nums[i] = product;
+            }else{
+                int temp = product / nums[i];
+                nums[i] = temp;
+            }
+        }
+        return nums;
+    }
+};
+
+
+
+/////// Using PrefixSum/Product Concept 
+/*
+    vector<int> productExceptSelf(vector<int>& nums) {
         
         int n = nums.size();
         vector<int> result(n,1);
@@ -18,4 +51,4 @@ public:
         }
         return result;
     }
-};
+*/
