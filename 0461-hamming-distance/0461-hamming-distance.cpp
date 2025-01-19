3dc42e48ada5c5ddde19c1799463695015036928
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-19 19:01:23 +0530

    Time: 0 ms (100%), Space: 8 MB (31.99%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         
-        int count =0;
-        while(x>0 || y>0){
-            int xbit = x&1, ybit = y&1;
-            if(xbit != ybit) {
-                count++;
-            }
-            x = x >> 1;
-            y = y >> 1;
+        int count =0; 
+        int n = x ^ y;   // the result n represents a binary number where each bit is 1 
+                        //if the corresponding bits in x and y are different, and 0 if they are the same. 
+        while(n){
+            count++;
+            n = n & n-1;   // concept written below
         }
         return count;
     }
 };
 
+/*
+    Concept of n &= n - 1:
+        Subtracting 1 from a number flips all the bits after the rightmost 1 in the binary representation, including the     rightmost 1 itself.
+    Performing n &= n - 1 clears the rightmost 1 in n.
+*/
 
 
 ////////////// 
@@ -23,25 +26,12 @@ public:
         
         int count =0;
         while(x>0 || y>0){
-            int xbit = x&1, ybit = y&1;
+            int xbit = x&1, ybit = y&1;  // Extracting individual bits from integers
             if(xbit != ybit) {
                 count++;
             }
-            x = x >> 1;
+            x = x >> 1;   //Removes the least significant bit by shifting bits to the right
             y = y >> 1;
-        }
-        return count;
-    }
-*/
-/*
-    int hammingDistance(int x, int y) {
-        
-        int count =0; 
-        int n = x ^ y;
-
-        while(n){
-            count++;
-            n = n & n-1;
         }
         return count;
     }
