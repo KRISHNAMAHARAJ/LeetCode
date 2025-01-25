d6add269ac6035678df954dc191c077981800881
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-26 01:41:39 +0530

    Time: 4 ms (47.17%), Space: 55 MB (48.24%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,23 +2,38 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         
-        int n = nums.size();
-           
-        sort(nums.begin(), nums.end());
-        int cand = 1;
-        for(int i=0; i< n; i++){
-            if(nums[i] > cand){
-                return cand;
-            }
-            else if(nums[i] == cand){
-                cand++;
+        int n = nums.size();                
+                                    //// Using Pigeonhole principle TC:- O(n) and SC:- O(1)
+        for(int i=0; i<n; i++){
+            if(nums[i] <= 0 || nums[i] > n){
+                nums[i] = n+1; 
             }
         }
-        return cand;
+
+        for(int i=0; i<n; i++){
+            int element = abs(nums[i]);
+            if(element == n+1){
+                continue;
+            }
+            int seat = element - 1;       // index == seat 
+            if(nums[seat] < 0){
+                continue;
+            }
+            nums[seat] = -nums[seat];
+        }
+
+        for(int i=0; i<n; i++){
+            if(nums[i] > 0){
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
 
+//Above method is optimal and best
 
+// test case:- [2,3,0,11,1,-7,3]  - this test case is good for making any logic
 
 
 ///////3rd Method:-  Using Hash Set TC:- O(n) and SC:- O(n) here extra space is used.
@@ -40,6 +55,27 @@ public:
     }
 */
 
+
+
+///////2nd Method:- Using Sorting  TC:- O(nlogn) and SC:- O(1) 
+/*
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+           
+        sort(nums.begin(), nums.end());
+        int cand = 1;
+        for(int i=0; i< n; i++){
+            if(nums[i] > cand){
+                return cand;
+            }
+            else if(nums[i] == cand){
+                cand++;
+            }
+        }
+        return cand;
+    }
+*/
 
 
 
