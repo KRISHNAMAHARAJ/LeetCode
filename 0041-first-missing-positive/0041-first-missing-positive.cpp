a1e9e156ee77e9f0448eed3b05f50234266e9bb5
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-26 00:54:07 +0530

    Time: 62 ms (16.22%), Space: 78.2 MB (15.96%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,6 +3,47 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         
         int n = nums.size();
+        unordered_set<int> st;
+
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+        }
+        for(int i = 1; i<=n+1; i++){
+            if(st.find(i) == st.end()){
+                return i;
+            }
+        }
+        return n+1;    
+    }
+};
+
+
+
+//////////// Using BRUTE FORCE TC:- O(n^2). But for large no.s of input it is difficult to process. So, its giving TLE. Only 172/178 test cases passed. 
+/*
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i = 1; i<=n+1; i++){
+            bool found = false;
+            for(int j=0; j<n; j++){
+                if(nums[j] == i){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                return i;
+            }  
+        }
+        return n+1;
+    }
+*/
+
+/*
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
     
     // Place each number at its correct index if possible
     for (int i = 0; i < n; i++) {
@@ -21,4 +62,4 @@ public:
     // If all numbers are in their correct places, return n + 1
     return n + 1;
     }
-};
+*/
