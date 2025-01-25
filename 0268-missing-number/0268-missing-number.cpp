a42665f31ee08f8e7503c65ba6b5227f3c48699f
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-01-26 02:10:12 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -41,3 +41,24 @@ public:
         return res; 
     }     
 };
+
+
+////// Using Binary Search after Sorting the array TC:- O(nlogn)
+/*
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]>mid)
+            {
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+*/
