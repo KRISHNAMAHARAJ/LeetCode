41979786806de8e3a5e7fc831455a6b60ccf0885
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-02-03 16:27:00 +0530

    Time: 0 ms (100%), Space: 17.9 MB (54.77%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        
-        for(int i=0; i<nums.size(); i++){          ///// NAIVE APPROACH TC:- O(n)
-            if(nums[i] == target) return true;
+    bool search(std::vector<int>& nums, int target) {
+
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return true;
+
+        ///This is the case for handling duplicate values rest all are same like q.no.33
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
         }
         return false;
     }
 };
 
-
-//////// USING BINARY SEARCH TC:- O(logn)
+/////NAIVE APPROACH;- USing Linear Search TC:- O(n)
 /*
-    bool search(std::vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) return true;
-
-            if (nums[low] == nums[mid]) {
-                low++;
-                continue;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
-                else low = mid + 1;
-            } else {
-                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
-                else high = mid - 1;
-            }
+    bool search(vector<int>& nums, int target) {
+        
+        for(int i=0; i<nums.size(); i++){          
+            if(nums[i] == target) return true;
         }
         return false;
     }
