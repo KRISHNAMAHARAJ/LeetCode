86a08248ed2c860e1d0488739bf2ad3ca89c9a7f
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-02-08 22:19:21 +0530

    Time: 0 ms (100%), Space: 16.2 MB (65.63%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -15,3 +15,42 @@ public:
         return nums[low];  
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////
+/*
+    int findMin(vector<int>& nums) {
+        
+        int low =0, high = nums.size() - 1;
+
+        while(low<high)
+        {
+            while(low<high && nums[low]==nums[high]) high--;
+            //Removal of Duplicates
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[high])low=mid+1;
+            else high=mid;
+        }
+        return nums[low];  
+    }
+*/
