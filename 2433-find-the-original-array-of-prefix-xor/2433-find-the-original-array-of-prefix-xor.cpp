71cb49d4c73178300ef16d395905f30a4101da73
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-02-15 10:15:47 +0530

    Time: 0 ms (100%), Space: 80.5 MB (48.48%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        int n = pref.size();
-        for(int i = n - 1; i > 0; i--) {
-            pref[i] ^= pref[i - 1];
+        
+        int n= pref.size();         // TC:- O(n) and SC:- O(n) if resultant array counts.
+        vector<int> arr(n,0);
+
+        arr[0] = pref[0];
+        for(int i=1; i<n; i++){
+            arr[i]= pref[i-1] ^ pref[i];
         }
-        return pref;
+        return arr;
     }
 };
 
