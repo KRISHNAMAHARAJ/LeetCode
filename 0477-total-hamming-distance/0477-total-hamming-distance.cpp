580a9a815335d8db395167e4ec1b2e643c87fc6e
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-02-23 22:38:33 +0530

    Time: 7 ms (79.68%), Space: 22.9 MB (50.49%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
         
+        int size = nums.size();
+        if(size < 2) return 0;
+        int ans = 0;
+        int *zeroOne = new int[2];
+        while(true)
+        {
+            int zeroCount = 0;
+            zeroOne[0] = 0;
+            zeroOne[1] = 0;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                if(nums[i] == 0) zeroCount++;
+                zeroOne[nums[i] % 2]++;
+                nums[i] = nums[i] >> 1;
+            }
+            ans += zeroOne[0] * zeroOne[1];
+            if(zeroCount == nums.size()) return ans;
+        }
+    }
+};
+
+
+
+///////// Brute force Approach :- TLE is coming ---39/46 test cases passed only
+/*
+    int totalHammingDistance(vector<int>& nums) {
+        
         int n= nums.size();
         int sum =0;
 
@@ -18,4 +45,4 @@ public:
         }
         return sum;
     }
-};
+*/
