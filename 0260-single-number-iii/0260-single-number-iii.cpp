1cbcceb9910d6341697342e6762bf27af5604a74
KRISHNA MAHARAJ NAYAN <128717160+KRISHNAMAHARAJ@users.noreply.github.com>
2025-03-02 10:12:32 +0530

    Time: 0 ms (100%), Space: 13.8 MB (81.99%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) { 
+
+        int xor1 = 0;           ///// Using Bitwise XOR concept TC:-O(n) and SC:- O(1) 
+        for(int num : nums){
+            xor1 ^= num;
+        }
+
+        int rightmost_set_Bit = xor1 & -(unsigned int)xor1;
+
+        int a=0, b=0;
+        for(int num: nums){
+            if(num & rightmost_set_Bit){
+                a ^= num;
+            }
+            else {
+                b ^= num;
+            }
+        }
+        return {a, b};
+    }
+};
+
+
+////////////// Using Frequency map TC:- O(n) and also SC:- O(n)
+/*
+    vector<int> singleNumber(vector<int>& nums) { 
         
         unordered_map<int, int> freq;
         for (int n : nums) {
@@ -16,4 +41,4 @@ public:
 
         return res;
     }
-};
+*/
